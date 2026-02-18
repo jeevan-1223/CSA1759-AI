d833473de61ed651e6b4cd3bcb62d6f324b3f794
jeevan-1223 <192472080.simats@saveetha.com>
2026-02-18 10:05:32 +0530

    Add files via upload

---
+++ exp24.pl
@@ -0,0 +1,10 @@
+% Facts: diet(Disease, Suggested_Diet)
+
+diet(diabetes, low_sugar).
+diet(bp, low_salt).
+diet(obesity, low_fat).
+diet(anemia, iron_rich_food).
+
+% Rule
+suggest_diet(Disease, Diet) :-
+    diet(Disease, Diet).
